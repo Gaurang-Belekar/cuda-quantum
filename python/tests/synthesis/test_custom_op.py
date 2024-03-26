f86f42082902ff7b36394a8951222f310f0c405c
Pradnya Khalate <pkhalate@nvidia.com>
2024-03-26 11:22:41 -0700

    * Basic tests to explore syntax and APIs

---
+++ python/tests/synthesis/test_custom_op.py
@@ -0,0 +1,45 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+import pytest
+import numpy as np
+import cudaq
+
+
+def check_basic(entity):
+    """Helper function to encapsulate checks"""
+    counts = cudaq.sample(entity, shots_count=100)
+    counts.dump()
+    assert '00' in counts and '11' in counts and len(counts) == 2
+
+
+def test_basic():
+    """
+    Showcase user-level APIs of how to 
+    (a) define a custom operation using unitary, and 
+    (b) how to use it in kernel
+    """
+    custom_h = cudaq.register_operation(1. / np.sqrt(2.) *
+                                        np.array([[1, 1], [1, -1]]))
+    custom_x = cudaq.register_operation(np.array([[0, 1], [1, 0]]))
+
+    @cudaq.kernel
+    def bell():
+        qubits = cudaq.qvector(2)
+        custom_h(qubits[0])
+        custom_x.ctrl(qubits[0], qubits[1])
+
+    check_basic(bell)
+
+    # Also works from builder
+    kernel = cudaq.make_kernel()
+    qubits = kernel.qalloc(2)
+    custom_h(qubits[0])
+    custom_x.ctrl(qubits[0], qubits[1])
+
+    check_basic(kernel)
---
+++ test/Synthesis/basic.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// RUN: cudaq-quake %cpp_std %s | FileCheck %s
+
+#include <cudaq.h>
+
+#include <cmath>
+#include <complex>
+#include <iostream>
+#include <vector>
+
+void custom_operation() __qpu__ {
+  cudaq::qvector qubits(2);
+  custom_h(qubits[0]);
+  custom_x.ctrl(qubits[0], qubits[1]);
+}
+
+int main() {
+  std::vector<std::complex<double>> my_h{{M_SQRT1_2, M_SQRT1_2},
+                                         {M_SQRT1_2, -M_SQRT1_2}};
+  std::vector<std::complex<double>> my_x{{0, 1}, {1, 0}};
+
+  auto custom_h = cudaq::register_operation(my_h);
+  auto custom_x = cudaq::register_operation(my_x);
+  
+  auto result = cudaq::sample(custom_operation);
+
+  std::cout << result.most_probable() << '\n';
+  return 0;
+}
